f2697463c3b838a44e2253b44198143c37115211
seba197878 <sebav1978@gmail.com>
2023-08-09 12:08:13 -0300

    Puse Clase Nombres

---
+++ src/grupo6/Nombres.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo6;
+
+/**
+ *
+ * @author seba
+ */
+public class Nombres {
+    
+    private String nombre;
+    private String apellido;
+    
+}
